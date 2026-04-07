8cc71299c7781358cd204c953899053a6fa94ade
Thanh <ktthanh1673@gmail.com>
2026-04-07 11:35:17 +0700

    fix(home): bind change-page event to fetchHomeData in MangaGrid

--- src/components/Home/MangaGrid.vue
+++ src/components/Home/MangaGrid.vue
@@ -4,16 +4,23 @@ import Pagination from './PaginationSession.vue'
 import MangaCard from '@/components/Common/MangaCard.vue'
 
 const homeStore = useHomeStore()
+
+// Thêm hàm xử lý chuyển trang
+const handlePageChange = async (newPage) => {
+  await homeStore.fetchHomeData(newPage)
+}
 </script>
 
 <template>
   <div>
-    <div v-if="homeStore.loading">...</div>
+    <div v-if="homeStore.loading" class="flex justify-center py-10">
+      <p class="text-gray-500 italic">Đang tải dữ liệu...</p>
+    </div>
 
     <div v-else class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-6">
       <MangaCard v-for="manga in homeStore.mangas" :key="manga._id" :manga="manga" />
     </div>
 
-    <Pagination />
+    <Pagination @change-page="handlePageChange" />
   </div>
 </template>
